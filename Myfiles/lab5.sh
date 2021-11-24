17010a137c4d75ff793a2892c9959217d4bd2235
Hozan <hozan1549@gmail.com>
2021-11-25 02:58:09 +0300

    Adding new file

--- Myfiles/lab5.sh
+++ Myfiles/lab5.sh
@@ -1,93 +1,103 @@
 #!/bin/bash
 
-# a=10
-# b=20
-# if [ $a != $b ]
-# then
-# 	echo "$a != $b : a is not equal to b"
-# else
-# 	echo "$a != $b : a is equal to b"
-# fi
+a=10
+b=20
+if [ $a != $b ]
+	then
+		echo "$a != $b : a is not equal to b"
+	else
+		echo "$a != $b : a is equal to b"
+fi
+echo "========================"
 
-# if [ $a -lt 100 -a $b -gt 15 ]
-# then 
-# 	echo "$a -lt 100 -a $b -gt 15: return true"
-# else
+# OR (-o), AND (-a) 
+if [ $a -lt 100 -a $b -gt 15 ]
+	then 
+		echo "$a -lt 100 -a $b -gt 15: return true"
+	else
 
-# 	echo "$a -lt 100 -a $b -gt 15: return false"
-# fi
-#  echo "========================"
+		echo "$a -lt 100 -a $b -gt 15: return false"
+fi
+echo "========================"
 
+# -z if a string is empty
+# -n if a string is not empty
 
-# a="abc"
-# b="efg"
-# # one equal if we compare string
-# if [ $a = $b ]
-# then
-# 	echo "$a != $b : a is equal to b"
-# else
-# 	echo "$a != $b : a is not equal to b"
-# fi
+str1=""
+str2="hello"
+if [ -z "$str1" ]
+	then
+		echo "str1 Is Empty"
+	else
+		echo "str1 is not Empty"
+fi
+if [ -n "$str1" ]
+	then
+		echo "str1 Is Empty"
+	else
+		echo "str1 is not Empty"
+fi
+echo "========================"
 
-# a=""
-# if [ -z $a ]
-# then 
-# 	echo "$a : String operand length is zero"
-# else
+a="abc"
+b="efg"
+# (=) or (==) it's the same if we use it for comparing string 
+if [ $a = $b ]
+	then
+		echo "$a != $b : a is equal to b"
+	else
+		echo "$a != $b : a is not equal to b"	
+fi
+if [ $a != $b ]
+	then
+		echo "$a != $b : a is not equal to b"
+	else
+		echo "$a != $b : a is equal to b"
+fi
+echo "========================"
 
-# 	echo "$a : String operand length is not zero"
-# fi
-# #  echo "========================"
+a=""
+if [ -z $a ]
+	then 
+		echo "$a : String operand length is zero"
+	else
 
-# file="./test.sh"
-# if [ -r $file ]
-# then
-# 	echo "File has read access"
-# else
-# 	echo "File does not have read access"
-# fi
+		echo "$a : String operand length is not zero"
+fi
+echo "========================"
 
+# (./) means open the file
+file="./test.sh"
+if [ -r $file ]
+	then
+		echo "File has read access"
+	else
+		echo "File does not have read access"
+fi
+echo "========================"
 
-# if [ $file ]
-# then 
-# 	echo "File exists"
-# else
+if [ $file ]
+	then 
+		echo "File exists"
+	else
 
-# 	echo "File does not exists"
-# fi
+		echo "File does not exists"
+fi
 
-# if [ -x $file ]
-# then
-# 	echo "File is executable"
-# else
-# 	echo "File is not executable"
-# fi
-#  echo "========================"
+if [ -f $file ]
+	then 
+		echo "File exists"
+	else
 
+		echo "File does not exists"
+fi
 
-# a=0
-# while [ "$a" -lt 10 ] # This is loop1
-# do
-# 	b="$a"
-# 	while [ "$b" -ge 0 ] # This loop2
-# 	do
-# 		echo -n "$b "
-# 		b=`expr $b - 1`
-# 	done
-# 	a=`expr $a + 1`
-# 	echo $a
-# done
-#  echo "========================"
-
-star=2
-for ((i=1;i<=9;i++))
-do
-	for ((j=1;j<=i;j++))
-	do
-		echo -n "$star"
-	done
-	echo
-done
-
+if [ -x $file ]
+	then
+		echo "File is executable"
+	else
+		echo "File is not executable"
+fi
+echo "========================"
 
 
